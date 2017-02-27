4cb723ecd1f06ca288016530ef1323d1a3441f12
anngoc2016 <anguyenngoc@fossil.com>
2017-02-27 10:35:21 +0700

    Update client.rb

--- lib/kong/client.rb
+++ lib/kong/client.rb
@@ -30,9 +30,9 @@ module Kong
       @api_url = url
       @http_client = Excon.new(self.api_url, omit_default_port: true)
     end
-    
-    def self.default_headers(headers = {})
-      @default_headers.merge(headers)
+   
+    def self.default_headers=(headers)
+      @default_headers = headers
     end
 
     def http_client
